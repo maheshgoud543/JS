e27f2615197bd0782d6e57bb84124756a8c0cba9
maheshgoud543 <85802360+maheshgoud543@users.noreply.github.com>
2024-06-11 16:48:16 +0000

    Variables Added

---
+++ Basics/Variables/Variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345;
+let firstName = "Mahesh";
+var lastName = "Goud";
+city = "Hyderabad";
+firstName = "Raju";
+lastName = "Alluri"
+city = "Bengaluru"
+
+console.table([accountId,firstName,lastName,city])
